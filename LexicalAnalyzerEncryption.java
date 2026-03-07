6a1040e0221229575029b5d7745b9d3871384117
Sushil Kumar Mishra <131292329+Sushil2k4@users.noreply.github.com>
2026-03-07 11:06:17 +0530

    Update LexicalAnalyzerEncryption.java

--- LexicalAnalyzerEncryption.java
+++ LexicalAnalyzerEncryption.java
@@ -1,1 +1,40 @@
+Problem Statement
 
+Write a Java program that acts as a simple lexical analyzer to encrypt a given paragraph of text.
+
+Each character in the text must be converted into a numeric value using the following mapping rules:
+
+Mapping Rules
+
+Lowercase letters:
+a = 1, b = 2, c = 3, ... , z = 26
+
+Uppercase letters:
+A = 27, B = 28, C = 29, ...
+
+Space " " = 0
+
+Full stop "." = 99
+
+Tasks
+
+Read the given paragraph of text.
+
+Convert each character into its encrypted numeric value according to the mapping rules.
+
+Display the encrypted numeric sequence.
+
+Calculate and display the sum of all encrypted values.
+
+Example
+
+Input
+
+COVID crisis
+
+Output
+
+29 41 48 35 30 0 3 18 9 19 9 19
+Language
+
+Java
