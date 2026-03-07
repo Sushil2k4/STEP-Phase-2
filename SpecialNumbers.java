105b0034fa1bb21dd507b378f4148092f9dbfa54
Sushil Kumar Mishra <131292329+Sushil2k4@users.noreply.github.com>
2026-03-07 11:53:40 +0530

    Update SpecialNumbers.java

--- SpecialNumbers.java
+++ SpecialNumbers.java
@@ -1,1 +1,40 @@
+Problem Statement
 
+A number is called a Special Number if the sum of its digits raised to the power of their respective positions (starting from 1 from the left) is equal to the number itself.
+
+For a number with digits:
+
+d₁ d₂ d₃ ... dₖ
+
+The number is Special if:
+
+d₁¹ + d₂² + d₃³ + ... + dₖᵏ = n
+
+Example
+
+Input:
+135
+
+Explanation:
+1¹ + 3² + 5³ = 1 + 9 + 125 = 135
+
+Output:
+Special Number
+
+Task
+
+Write a Java program to:
+
+Identify all Special Numbers between 1 and 10,000
+
+Display all such numbers
+
+Print the total count of Special Numbers found
+
+Constraints
+
+1 ≤ n ≤ 10,000
+
+Position counting starts from the leftmost digit
+
+Zero digits contribute 0^position = 0
