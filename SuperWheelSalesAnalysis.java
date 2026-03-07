55ebc3c41c00027c460443d0ba701a1bd782e7b6
Sushil Kumar Mishra <131292329+Sushil2k4@users.noreply.github.com>
2026-03-07 10:29:18 +0530

    Implement daily sales calculation and corporate day check
    Added methods to calculate daily sales and identify corporate sales days.

--- SuperWheelSalesAnalysis.java
+++ SuperWheelSalesAnalysis.java
@@ -25,3 +25,54 @@ Functions / Methods
 Conditional statements
 
 Mathematical computation
+
+  ********************************************************************************************************
+
+  import java.util.*;
+
+public class SuperWheelSalesAnalysis {
+
+    public static int dailySales(int day) {
+        return (day * day - day + 1);
+    }
+    public static boolean isCorporateDay(int day) {
+        return (day % 5 == 0);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        String[] months = {"April", "May", "June", "July", "August", "September"};
+        int[] daysOfMonth = {30,31,30,31,31,30};
+        int[] monthlySales = new int[6];
+        int retailTotal = 0;
+        int corporateTotal = 0;
+        int dayCount = 1;
+        for (int month = 0; month < 6; month ++) {
+            for (int days = 1; days <= daysOfMonth[month]; days++) {
+                int sales = dailySales(dayCount);
+                monthlySales[month] += sales;
+                if (isCorporateDay(dayCount)) {
+                    corporateTotal += sales;
+                } else {
+                    retailTotal += sales;
+                }
+                dayCount++;
+            }
+        }
+        System.out.println("Total vehicles sold per month :");
+        for (int month = 0; month < 6; month++) {
+            System.out.println(months[month] + " generated : " + monthlySales[month]);
+        }
+        System.out.println("\n");
+        System.out.println("Cumulative sales by customer type");
+        System.out.println("Total Retail sales :" + retailTotal);
+        System.out.println("Total Corporate sales : " + corporateTotal);
+        System.out.println("\n");
+        System.out.println("Total Sales :" + (retailTotal + corporateTotal));
+    }
+}
+
+
+
+
