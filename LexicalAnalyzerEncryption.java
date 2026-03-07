0447b5666240e97e63f7c72a95a3fe9b5dbb4ce1
Sushil Kumar Mishra <131292329+Sushil2k4@users.noreply.github.com>
2026-03-07 11:06:56 +0530

    Implement lexical analyzer for text encryption

--- LexicalAnalyzerEncryption.java
+++ LexicalAnalyzerEncryption.java
@@ -40,3 +40,38 @@ Language
 Java
 
   ***********************************************************************
+
+ public class LexicalAnalyzer {
+    public static void main(String[] args) {
+        String text = "COVID crisis has taught us several lessons and together we have overcome this great challenge."
+        + "The COVID pandemic sent shock waves through the world economy and triggered the largest global economic crisis in more than a century."
+        + "The crisis led to a dramatic increase in inequality within and across countries."
+        + "Preliminary evidence suggests that the recovery from the crisis will be as uneven as its initial economic impacts, with emerging economies and economically disadvantaged groups needing much more time to recover from pandemic-induced losses of income and livelihoods."
+        + "Now there is a sense of introspection in people. India has emerged stronger";
+
+        int sum = 0;
+        System.out.println("The Encrypted statement is : ");
+
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            int value = 0;
+
+            if (ch >= 'a' && ch <= 'z') {
+                value = ch - 'a' + 1;
+            } else if ( ch >= 'A' && ch <= 'Z') {
+                value = ch - 'A' + 27;
+            } else if ( ch == ' ') {
+                value  = 0;
+            } else if ( ch == '.') {
+                value = 99;
+            } else {
+                continue;
+            }
+            System.out.print(value + " ");
+            sum += value;
+        }
+        System.out.println("\n");
+        System.out.println("Sum of all encrypted numbers : " + sum);
+    }
+}
+
