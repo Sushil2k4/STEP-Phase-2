38a9cc3a809a64195f78cffa1228871d572baef0
Sushil Kumar Mishra <131292329+Sushil2k4@users.noreply.github.com>
2026-03-08 18:31:44 +0530

    Update SuperWheelSalesAnalysis.java

--- SuperWheelSalesAnalysis.java
+++ SuperWheelSalesAnalysis.java
@@ -77,5 +77,59 @@ public class SuperWheelSalesAnalysis {
 Enhancement : Print the sales from July 15 to September 15 
 
 
+import java.util.*;
+
+public class SuperWheelSalesAnalysis {
+
+    public static int dailySales(int day) {
+        return (day * day - day + 1);
+    }
+    public static boolean isCorporateDay(int day) {
+        return (day % 5 == 0);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        String[] months = {"April", "May", "June", "July", "August", "September"};
+        int[] daysOfMonth = {30,31,30,31,31,30};
+        int[] monthlySales = new int[6];
+        int retailTotal = 0;
+        int corporateTotal = 0;
+        int dayCount = 1;
+        int midPeriodSales = 0;
+        for (int month = 0; month < 6; month ++) {
+            for (int days = 1; days <= daysOfMonth[month]; days++) {
+                int sales = dailySales(dayCount);
+                monthlySales[month] += sales;
+                if (isCorporateDay(dayCount)) {
+                    corporateTotal += sales;
+                } else {
+                    retailTotal += sales;
+                }
+                if ((month == 3 && days >= 15) || (month == 4) || (month == 5 && days <= 15)) {
+                    midPeriodSales += sales;
+                }
+                dayCount++;
+            }
+        }
+        System.out.println("Total vehicles sold per month :");
+        for (int month = 0; month < 6; month++) {
+            System.out.println(months[month] + " generated : " + monthlySales[month]);
+        }
+        System.out.println("\n");
+        System.out.println("Cumulative sales by customer type");
+        System.out.println("Total Retail sales :" + retailTotal);
+        System.out.println("Total Corporate sales : " + corporateTotal);
+        System.out.println("\n");
+        System.out.println("Total Sales :" + (retailTotal + corporateTotal));
+        System.out.println("\n");
+        System.out.println("Total Number of Sales between July 15 and September 15 : " + midPeriodSales);
+    }
+}
+
+
+
+
 
 
